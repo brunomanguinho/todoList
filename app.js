fdec03ae47d8293b018024659d6b07abed71113c
brunomanguinho <brunomanguinho@gmail.com>
2022-02-27 08:37:48 -0300

    Preparing for heroku

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+// mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://brunomanguinho:Trooper1983@cluster0.xbjht.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 //Schemas & Models
 const itemsSchema = {
@@ -169,6 +170,11 @@ app.post("/delete", function(req, res){
 
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started succesfully");
 });
